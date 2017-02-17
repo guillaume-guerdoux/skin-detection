a8af9b9942cde2449250d011f063daf4b7002c79
Guillaume <guillaume.guerdoux@hotmail.fr>
2017-02-17 18:33:22 +0100

    Implement histogram creation in utils.py

--- utils.py
+++ utils.py
@@ -12,8 +12,35 @@ def load_image(nom, is_color):
     return img
 
 
+def inverse_image(I):
+    # I : cv2 img
+    return 255 - I
+
+
+def BGR_to_HSV(img):
+    return cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+
+def BGR_to_Lab(img):
+    return cv2.cvtColor(img, cv2.CV_BGR2Lab)
+
+
+# http://www.pyimagesearch.com/2014/01/22/clever-girl-a-guide-to-utilizing-color-histograms-for-computer-vision-and-image-search-engines/
+def BGR_histogram(bgr_img, black_and_white_img):
+    # plot a 2D color histogram for red and green
+    hist = cv2.calcHist([bgr_img], [2, 1], black_and_white_img[:, :, 0],
+                        [32, 32], [0, 256, 0, 256])
+    return hist
+
+
 if __name__ == '__main__':
-    img = load_image("data/lenna.jpg", True)
-    cv2.imshow('image', img)
+    # data/Ground_Truth/GroundT_FacePhoto/06Apr03Face.png
+    # data/Pratheepan_Dataset/FacePhoto/06Apr03Face.jpg
+    bgr_img = load_image("data/Pratheepan_Dataset/FacePhoto/06Apr03Face.jpg",
+                         True)
+    black_and_white_img = load_image(
+        "data/Ground_Truth/GroundT_FacePhoto/06Apr03Face.png", True)
+    '''cv2.imshow('image', img)
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    cv2.destroyAllWindows()'''
+    BGR_skin_histogram(bgr_img, black_and_white_img)
