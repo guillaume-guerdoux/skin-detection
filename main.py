46655bb4751bf2e3f0253dd57435fa5e78aed591
Guillaume <guillaume.guerdoux@hotmail.fr>
2017-02-17 19:40:57 +0100

    Implement create skin and non skin model

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from skin_detector import ExplicitSkinDetector
+from skin_detector import NonParametricSkinDetector
 from utils import *
 
 import numpy as np
@@ -7,8 +8,8 @@ from matplotlib import pyplot as plt
 import cv2
 
 if __name__ == '__main__':
-    explicit_skin_detector = ExplicitSkinDetector()
-    '''folder = "data/Pratheepan_Dataset/FacePhoto/"
+    '''explicit_skin_detector = ExplicitSkinDetector()
+    folder = "data/Pratheepan_Dataset/FacePhoto/"
     for filename in os.listdir(folder):
         img = load_image(folder + filename, True)
         cv2.imshow('image', img)
@@ -19,7 +20,7 @@ if __name__ == '__main__':
         )
         cv2.imshow('image', skin_img)
         cv2.waitKey(0)
-        cv2.destroyAllWindows()'''
+        cv2.destroyAllWindows()
     img = load_image('data/lenna.jpg', True)
     cv2.imshow('image', img)
     cv2.waitKey(0)
@@ -29,4 +30,7 @@ if __name__ == '__main__':
     )
     cv2.imshow('image', skin_img)
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    cv2.destroyAllWindows()'''
+    skin_detector = NonParametricSkinDetector()
+    skin_detector.create_skin_models([("data/Pratheepan_Dataset/FacePhoto/",
+                                      "data/Ground_Truth/GroundT_FacePhoto/")])
--- skin_detector.py
+++ skin_detector.py
@@ -1,6 +1,8 @@
 import numpy as np
+import os
 from matplotlib import pyplot as plt
 import cv2
+from utils import *
 
 
 class ExplicitSkinDetector:
@@ -29,3 +31,33 @@ class ExplicitSkinDetector:
                 else:
                     img[index_row][index_triplet] = color_for_not_skin
         return img
+
+
+class NonParametricSkinDetector:
+
+    def create_skin_models(self, folders):
+        '''folder : lists of differents sample folders
+        Two arrays dimension (a and b) : 0 -> 256 0 -> 256
+        One array skin model : nb of skin pixel in image
+        One array non skin model : nb of non skin pixel in image'''
+        skin_models = np.zeros((32, 32))
+        non_skin_models = np.zeros((32, 32))
+        for folder in folders:
+            for filename in os.listdir(folder[0]):
+                color_img = load_image(folder[0] + filename, True)
+                black_and_white_img = load_image(
+                    folder[1] + os.path.splitext(filename)[0]+'.png', True)
+                inversed_black_and_white_img = \
+                    inverse_image(black_and_white_img)
+                temp_skin_histogram = BGR_histogram(
+                    color_img, black_and_white_img
+                )
+                skin_models = np.add(skin_models, temp_skin_histogram)
+                temp_non_skin_histogram = BGR_histogram(
+                    color_img, inversed_black_and_white_img
+                )
+                non_skin_models = np.add(
+                    non_skin_models, temp_non_skin_histogram
+                )
+        skin_models = np.divide(skin_models, sum(sum(skin_models)))
+        non_skin_models = np.divide(non_skin_models, sum(sum(non_skin_models)))
